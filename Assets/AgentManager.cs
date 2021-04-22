a8d0ca79a24aae3d0f80088ab82f467f19f569b1
LuaMartins <luamartins070@gmail.com>
2021-04-22 18:34:58 -0300

    Initial commit

---
+++ Assets/AgentManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai"); // função criada para atribuir na unity
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // condição para orientação do click do mouse
+        {
+            RaycastHit hit; // controle do ambiente para navegação
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) // marca e posiciona para disparar o hit para trafego
+            {
+                foreach (GameObject a in agents) // para repetir a transição
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
